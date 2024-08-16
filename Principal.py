7251a7b423821590439c92388e0ee2b00f990805
Drew_Anjos <113213499+Drew-anjos@users.noreply.github.com>
2024-08-16 20:42:21 -0300

    Add files via upload

---
+++ Principal.py
@@ -0,0 +1,10 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(1089,297,duration=2)
+pyautogui.write("andrew")
+
+pyautogui.click(1084,323,duration=1)
+pyautogui.write("1234")
+
+pyautogui.click(947,354,duration=1)
